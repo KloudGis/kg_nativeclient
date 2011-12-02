eab4f5de614d51602aa444cd31d0ea5e36ba432f
Jean-Félix Girard <jeanfelixg@gmail.com>
2011-12-02 15:13:27 -0500

    test camera

---
+++ phonegap/phonegap_tests/camera/test/test/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  test
+//
+//  Created by Jean-Félix Girard on 11-12-02.
+//  Copyright Echosphere technologies 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
